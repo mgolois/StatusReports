e33f1440114961fcdc54212bf17aa923c183025a
dedgar1 <jnzokwe@gmail.com>
2016-05-04 13:37:50 -0400

    Added front end validation to hours fields in edit and create

--- src/StatusReports/wwwroot/js/site.js
+++ src/StatusReports/wwwroot/js/site.js
@@ -1,4 +1,23 @@
 ﻿// Write your Javascript code.
 $(document).ready(function () {
-    
+    $("#editForm").validate();
+    $("#createForm").validate({
+        onfocusout: false,
+        errorClass: "error",
+        validClass: "validEntry",
+        messages: {
+            mondayHours: {
+                min: " number > 0 ",
+                max: " number < 24"
+            }
+            
+        },
+        invalidHandler: function(form, validator){
+            var errors = validator.numberOfInvalids();
+            if(errors){
+                validator.errorList[0].element.focus();
+            }
+        }
+        
+    });
 });
